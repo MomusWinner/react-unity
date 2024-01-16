ae632680f393474b6a12344ad98008c9081e3842
MomusWinner <areg06.06@mail.ru>
2024-01-16 11:11:52 +0300

    remove ws from App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import {UnityContainer} from "./components/UnityContainer/UnityContainer";
 
-//enter your server
-const socket = new WebSocket("ws://localhost:9000");
 
 function App() {
   return (
     <div className="App">
-      <UnityContainer socket={socket}/>
+      <UnityContainer/>
     </div>
   );
 }
